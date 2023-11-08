5c81eff9edf024cc7711a271386dfdea0cf90466
Brittany Brimley <brittanysapp@gmail.com>
2023-11-08 03:28:54 -0700

    added Navigation.jsx component per requested criteria

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,9 @@
+export default function Navigation({ links }) {
+    return (
+        <ul className='navbar-nav ms-auto'>
+            {links.map((link, index) => (
+                <li key={index} className='nav-link'>{link}</li>
+            ))}
+        </ul>
+    );
+}
