83126d3a4b6296d70b9dc4d9cf6af85d87a59a76
Brittany Brimley <brittanysapp@gmail.com>
2023-11-08 05:46:52 -0700

    added file structure and ToDo items for AboutMe page

---
+++ src/views/AboutMe.jsx
@@ -0,0 +1,16 @@
+const styles = {
+    // TODO: Add inline CSS
+};
+
+function AboutMe() {
+    return (
+        <div className=''>
+            <h1>About Me</h1>
+        </div>
+        // TODO: Add styling and formatting with react-bootstrap
+        // TODO: Add profile pic
+        // TODO: Add <p></p> tag for AboutMe content
+    )
+}
+
+export default AboutMe;
