d8fd61e01570f5010300725b4520e3408b47a162
Brittany Brimley <brittanysapp@gmail.com>
2023-11-08 19:43:47 -0700

    added file structure and ToDo items for Contact page

---
+++ src/views/ContactMe.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+const styles = {
+    // TODO: Add inline CSS
+};
+
+function Contact() {
+    // TODO: Contact Form with fields for a name, an email address, and a message
+    // TODO: Notification that field is required when required field is missing
+    // TODO: Set up email validator and create notification when invalid email is entered
+}
+
+export default Contact;
