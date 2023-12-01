3cdb0f620d54182e2f5a341c182458594658acad
Brittany Brimley <brittanysapp@gmail.com>
2023-12-01 10:24:15 -0700

    added iframe resume

--- src/views/Resume/Resume.jsx
+++ src/views/Resume/Resume.jsx
@@ -8,6 +8,9 @@ function Resume() {
         <div className=''>
             <h1>Resume</h1>
             <a href={resume} target='_blank'>Resume</a>
+            <div className=''>
+                <iframe width="100%" height="600" src={resume}></iframe>
+            </div>
         </div>
         // TODO: Add styling and formatting with react-bootstrap
         // TODO: Add profile pic
