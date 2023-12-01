873177a3a98ba6f38b3eb5e1050e7d32cfc31e5f
Brittany Brimley <brittanysapp@gmail.com>
2023-12-01 12:36:38 -0700

    added styling and images to Portfolio page

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './style.css'
 
 function Project({ title, screenshot, url }) {
     return (
-        <div>
+        <div className='image-grid'>
             <a href={url} target="_blank">
-                <img src={screenshot} alt={title} />
+                <img src={screenshot} alt={title} className='image-box'/>
                 <h3>{title}</h3>
             </a>
         </div>
--- src/views/Portfolio/Portfolio.jsx
+++ src/views/Portfolio/Portfolio.jsx
@@ -4,37 +4,43 @@
 // import 'swiper/css/effect-coverflow';
 import './style.css'
 import Project from '../../components/Projects/Project';
+import masterBuilder from '../../assets/images/masterBuilderScreenshot.png';
+import theWeb from '../../assets/images/theWeb.png';
+import concertGroupie from '../../assets/images/concertGroupie.png';
+import novelPursuit from '../../assets/images/novelPursuit.png';
+import scribeMaster from '../../assets/images/scribeMaster.png';
+import weatherDashboard from '../../assets/images/weatherDashboard.png';
 
 const projects = [
     {
         // TODO: Add four more Project titles and all Screenshots and URLs for project websites
         title: 'masterBuilder',
-        screenshot: '',
+        screenshot: masterBuilder,
         url: 'https://master-builder-production-40c9.up.railway.app/',
     },
     {
+        title: 'theWeb',
+        screenshot: theWeb,
+        url: 'https://theweb-production.up.railway.app/',
+    },
+    {
         title: 'Concert Groupie',
-        screenshot: '',
+        screenshot: concertGroupie,
         url: 'https://shindekokoro.github.io/concert-groupie/',
     },
     {
         title: 'Novel Pursuit',
-        screenshot: '',
+        screenshot: novelPursuit,
         url: 'https://novel-pursuit-production.up.railway.app/',
     },
     {
         title: 'Scribe Master 3000',
-        screenshot: '',
+        screenshot: scribeMaster,
         url: 'https://scribe-master-3000-production.up.railway.app/',
     },
     {
-        title: 'Micromanager Deluxe',
-        screenshot: '',
-        url: 'https://github.com/Git-BritHub/Micromanager-Deluxe.git',
-    },
-    {
         title: 'Destination Weather Dashboard',
-        screenshot: '',
+        screenshot: weatherDashboard,
         url: 'https://git-brithub.github.io/Destination-Weather-Dashboard/',
     },
 ];
@@ -43,9 +49,11 @@ function Portfolio () {
     return (
         <div>
         <h1>Projects</h1>
-        {projects.map((project, index) => (
-            <Project key={index} {...project} />
-        ))}
+        <div className='image-grid'>
+            {projects.map((project, index) => (
+                <Project key={index} {...project} />
+            ))}
+        </div>
     </div>
         // <Swiper
         //     grabCursor
