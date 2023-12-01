3bcf09fbdd770902f60088917bb7537d2e05c4b3
Brittany Brimley <brittanysapp@gmail.com>
2023-12-01 09:12:25 -0700

    added more software skills in About Me section

--- src/views/AboutMe/AboutMe.jsx
+++ src/views/AboutMe/AboutMe.jsx
@@ -12,7 +12,7 @@ function AboutMe() {
                 className='profilePic justify-content-center align-items-center mt-5 mb-5'>
             </img>
             <h2>FULL STACK WEB DEVELOPER</h2>
-            <h3>JavaScript | CSS | HTML | Node.js | Express.js | Handlebars | MySQL | MERN | MongoDB | React </h3>
+            <h3>JavaScript | CSS | HTML | NodeJS | JSON | JQuery | ExpressJS | Handlebars | MySQL | MERN | MongoDB | GraphQL | ReactJS </h3>
             <h3>Bootstrap | Tailwind | Bulma | Figma</h3>
             <p>Hardworking software developer who gives great attention to detail while taking pride and care in the work that I do. Incredibly self-motivated with a curious mind and great love of learning. Constantly looking for ways to enhance my skills, efficiency and knowledge of our ever-changing and evolving field. Charismatic with excellent communication skills who excels in a team environment, as well as individually. Prioritizes and strives to meet a company’s goals and further its success.  </p>
         </div>
