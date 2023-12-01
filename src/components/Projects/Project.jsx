2fc20a4d51290cd2828f418b811027817deeaf83
Brittany Brimley <brittanysapp@gmail.com>
2023-12-01 13:04:21 -0700

    completed styling for portfolio page until I can figure out swiper carousel

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -3,10 +3,12 @@ import './style.css'
 
 function Project({ title, screenshot, url }) {
     return (
-        <div className='image-grid'>
+        <div className='image-box'>
             <a href={url} target="_blank">
-                <img src={screenshot} alt={title} className='image-box'/>
+                <img src={screenshot} alt={title} />
+                <div className='overlay'>
                 <h3>{title}</h3>
+            </div>
             </a>
         </div>
     );
