2c1efac4e395ec327d78282fdd1e69fe56013841
Brittany Brimley <brittanysapp@gmail.com>
2023-12-01 11:54:07 -0700

    minor edit for resume URL and deleted completed TODO comments

--- src/views/Resume/Resume.jsx
+++ src/views/Resume/Resume.jsx
@@ -7,14 +7,12 @@ function Resume() {
     return (
         <div className=''>
             <h1>Resume</h1>
-            <a href={resume} target='_blank'>Resume</a>
+            <a href={resume} target='_blank'>URL for Resume</a>
             <div className=''>
                 <iframe width="100%" height="600" src={resume}></iframe>
             </div>
         </div>
         // TODO: Add styling and formatting with react-bootstrap
-        // TODO: Add profile pic
-        // TODO: Add <p></p> tag for AboutMe content
     )
 }
 
