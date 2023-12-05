f4316efb31f1cd868420256a5209de9405e56622
Brittany Brimley <brittanysapp@gmail.com>
2023-12-05 12:05:00 -0700

    styling edits

--- src/views/AboutMe/AboutMe.jsx
+++ src/views/AboutMe/AboutMe.jsx
@@ -1,8 +1,8 @@
 import profilePic from '../../assets/images/profile_pic.png'
 import './style.css';
-const styles = {
-    // TODO: Add inline CSS
-};
+
+// const styles = {
+// };
 
 function AboutMe() {
     const imagePath = profilePic;
@@ -16,9 +16,7 @@ function AboutMe() {
             <h3>Bootstrap | Tailwind | Bulma | Figma</h3>
             <p>Hardworking software developer who gives great attention to detail while taking pride and care in the work that I do. Incredibly self-motivated with a curious mind and great love of learning. Constantly looking for ways to enhance my skills, efficiency and knowledge of our ever-changing and evolving field. Charismatic with excellent communication skills who excels in a team environment, as well as individually. Prioritizes and strives to meet a company’s goals and further its success.  </p>
         </div>
-        // TODO: Add styling and formatting with react-bootstrap
-        // TODO: Add profile pic
-        // TODO: Add <p></p> tag for AboutMe content
+        // TODO: Add Parallax feature
     )
 }
 
--- src/views/Resume/Resume.jsx
+++ src/views/Resume/Resume.jsx
@@ -1,18 +1,15 @@
 import resume from '../../assets/files/Brimley_Resume.pdf';
-const styles = {
-    // TODO: Add inline CSS
-};
+import './style.css';
 
 function Resume() {
     return (
         <div className='mt-5 pt-3'>
             <h1>RESUME</h1>
-            <a href={resume} target='_blank'>RESUME URL</a>
-            <div className='mt-3'>
+            <a href={resume} target='_blank' className='resumeUrl'>CLICK HERE FOR RESUME URL</a>
+            <div className='border mt-3 mx-5'>
                 <iframe width="100%" height="600" src={resume}></iframe>
             </div>
         </div>
-        // TODO: Add styling and formatting with react-bootstrap
     )
 }
 
