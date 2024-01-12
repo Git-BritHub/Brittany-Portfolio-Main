092d6f8ea1caff1fa665015c2122518bc5b82c27
Brittany Brimley <brittanysapp@gmail.com>
2024-01-12 00:05:33 -0700

    updated about me summary

--- src/views/AboutMe/AboutMe.jsx
+++ src/views/AboutMe/AboutMe.jsx
@@ -12,9 +12,9 @@ function AboutMe() {
                 className='profilePic justify-content-center align-items-center mt-5 mb-5'>
             </img>
             <h2>FULL STACK WEB DEVELOPER</h2>
-            <h3>JavaScript | CSS | HTML | NodeJS | JSON | JQuery | ExpressJS | Handlebars | MySQL | MERN | MongoDB | GraphQL | ReactJS </h3>
-            <h3>NextJS | Tailwind | Bootstrap | Bulma | Figma</h3>
-            <p>Hardworking software developer who gives great attention to detail while taking pride and care in the work that I do. Incredibly self-motivated with a curious mind and great love of learning. Constantly looking for ways to enhance my skills, efficiency and knowledge of our ever-changing and evolving field. Charismatic with excellent communication skills who excels in a team environment, as well as individually. Prioritizes and strives to meet a company’s goals and further its success.  </p>
+            <h3>JavaScript | CSS | HTML | NodeJS | JSON | JQuery | ExpressJS | Handlebars | JWT | MySQL | MERN | MongoDB | GraphQL | Apollo </h3>
+            <h3>React | NextJS | Tailwind CSS | Bootstrap | Bulma | Figma</h3>
+            <p>Diligent software developer with a keen artistic eye, committed to delivering exceptional work with meticulous attention to detail. Highly self-motivated, driven by curiosity, and possess a deep passion for continuous learning in our dynamic and ever-evolving field. Proactively seeking opportunities to elevate my skills and efficiency. Excellent problem-solver with an innovative and strategic mindset. Excels in fostering positive and communicative team environments, contributing effectively. Simultaneously thrives in independent work settings and is committed to realizing a company's objectives with a focus on delivering impactful results.</p>
         </div>
         // TODO: Add Parallax feature
     )
