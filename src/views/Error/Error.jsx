604824f6c811d3e901706f618a9ac210b563b4a4
Brittany Brimley <brittanysapp@gmail.com>
2024-01-20 02:53:55 -0700

    added margin and padding to Error page

--- src/views/Error/Error.jsx
+++ src/views/Error/Error.jsx
@@ -6,7 +6,7 @@ export default function Error() {
 
     return (
         <div id="error">
-            <h1>Ruh Roh...</h1>
+            <h1 className="py-5 mt-5">Ruh Roh...</h1>
             <p>An unexpected error has occurred.</p>
             <p>
                 <i>{error.statusText || error.message}</i>
