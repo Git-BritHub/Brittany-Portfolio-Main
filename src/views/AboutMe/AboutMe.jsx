146fb7cb6b5f10f934247fc6a9306d33217f89dc
Brittany Brimley <brittanysapp@gmail.com>
2024-02-18 19:20:47 -0700

    added more skills to about me section

--- src/views/AboutMe/AboutMe.jsx
+++ src/views/AboutMe/AboutMe.jsx
@@ -14,8 +14,12 @@ function AboutMe() {
                 effect="opacity"
             />
             <h2>FULL STACK WEB DEVELOPER</h2>
-            <h3>JavaScript | TypeScript | CSS | HTML | NodeJS | JSON | JQuery | ExpressJS | Handlebars | JWT | MySQL | MERN | MongoDB | GraphQL | Apollo </h3>
-            <h3>React | NextJS | DaisyUI | Tailwind CSS | Bootstrap | Bulma | Figma | Netlify</h3>
+            <h3>
+                JavaScript | TypeScript | CSS | HTML | NodeJS | JSON | JQuery | ExpressJS | Handlebars | JWT | MySQL | MERN | MongoDB | GraphQL | Apollo 
+            </h3>
+            <h3>
+                React | NextJS | DaisyUI | Tailwind CSS | Bootstrap | Bulma | Figma | Netlify | Swiper | GSAP    
+            </h3>
             <p>Diligent software developer with a keen artistic eye, committed to delivering exceptional work with meticulous attention to detail. Highly self-motivated, driven by curiosity, and possess a deep passion for continuous learning in our dynamic and ever-evolving field. Proactively seeking opportunities to elevate my skills and efficiency. Excellent problem-solver with an innovative and strategic mindset. Excels in fostering positive and communicative team environments, contributing effectively. Simultaneously thrives in independent work settings and is committed to realizing a company's objectives with a focus on delivering impactful results.</p>
         </div>
         // TODO: Add Parallax feature
