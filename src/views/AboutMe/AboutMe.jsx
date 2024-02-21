5de94664b2676bfa0ebe74a9a888ebe88b30e431
Brittany Brimley <brittanysapp@gmail.com>
2024-02-20 22:51:50 -0700

    fixed tags for SEO in About me page

--- src/views/AboutMe/AboutMe.jsx
+++ src/views/AboutMe/AboutMe.jsx
@@ -13,14 +13,14 @@ function AboutMe() {
                 className='profilePic justify-content-center align-items-center mt-5 mb-5'
                 effect="opacity"
             />
-            <h2>FULL STACK WEB DEVELOPER</h2>
-            <h3>
-                JavaScript | TypeScript | MERN Stack | HTML | NodeJS | JSON | JQuery | ExpressJS | Handlebars | JWT | MySQL | MongoDB | GraphQL | Apollo 
-            </h3>
-            <h3>
-                React | NextJS | DaisyUI | CSS | Tailwind CSS | Bootstrap | Bulma | Figma | Netlify | Swiper | GSAP    
-            </h3>
-            <p>Diligent software developer with a keen artistic eye, committed to delivering exceptional work with meticulous attention to detail. Highly self-motivated, driven by curiosity, and possess a deep passion for continuous learning in our dynamic and ever-evolving field. Proactively seeking opportunities to elevate my skills and efficiency. Excellent problem-solver with an innovative and strategic mindset. Excels in fostering positive and communicative team environments, contributing effectively. Simultaneously thrives in independent work settings and is committed to realizing a company's objectives with a focus on delivering impactful results.</p>
+            <h1>FULL STACK WEB DEVELOPER</h1>
+            <h2>
+                JavaScript | TypeScript | MERN Stack | HTML | NodeJS | JSON     JQuery | ExpressJS | Handlebars | JWT | MySQL | MongoDB | GraphQL | Apollo | React | NextJS | DaisyUI | CSS | Tailwind CSS | Bootstrap | Bulma | Figma | Netlify | Swiper | GSAP    
+            </h2>
+
+            <p>
+                Diligent software developer with a keen artistic eye, committed to delivering exceptional work with meticulous attention to detail. Highly self-motivated, driven by curiosity, and possess a deep passion for continuous learning in our dynamic and ever-evolving field. Proactively seeking opportunities to elevate my skills and efficiency. Excellent problem-solver with an innovative and strategic mindset. Excels in fostering positive and communicative team environments, contributing effectively. Simultaneously thrives in independent work settings and is committed to realizing a company's objectives with a focus on delivering impactful results.
+            </p>
         </div>
         // TODO: Add Parallax feature
     )
