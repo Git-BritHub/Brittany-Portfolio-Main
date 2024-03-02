2f6951eade4e9705e4759ca1aa1de31f11741fd4
Brittany Brimley <brittanysapp@gmail.com>
2024-03-02 16:17:59 -0700

    margin adjustements so that resume displays correctly on smaller devices

--- src/views/Resume/Resume.jsx
+++ src/views/Resume/Resume.jsx
@@ -3,11 +3,11 @@ import './style.css';
 
 function Resume() {
     return (
-        <div className='mt-5 pt-3'>
+        <div className='mt-5 pt-3 mx-5'>
             <h1 className="mt-5">RESUME</h1>
             <a href={resume} target='_blank' className='resumeUrl'>CLICK HERE FOR RESUME URL</a>
-            <div className='border mt-3 mx-5'>
-                <iframe width="100%" height="600" src={resume}></iframe>
+            <div className='border mt-3'>
+                <iframe width="100%" height="1000" src={resume}></iframe>
             </div>
         </div>
     )
