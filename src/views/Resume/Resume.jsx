a9370c5018d5bb1785bc0753c9b75b805620bcc7
Brittany Brimley <brittanysapp@gmail.com>
2024-03-02 19:32:54 -0700

    edited resume section to improve look on various screen sizes

--- src/views/Resume/Resume.jsx
+++ src/views/Resume/Resume.jsx
@@ -3,11 +3,11 @@ import './style.css';
 
 function Resume() {
     return (
-        <div className='mt-5 pt-3 mx-5'>
-            <h1 className="mt-5">RESUME</h1>
+        <div className='my-5 pt-3 pb-5 mx-3' style={{ height: '78vh' }}>
+            <h1 className="mt-5 mb-3">RESUME</h1>
             <a href={resume} target='_blank' className='resumeUrl'>CLICK HERE FOR RESUME URL</a>
-            <div className='border mt-3'>
-                <iframe width="100%" height="1000" src={resume}></iframe>
+            <div className='container-fluid mt-4 d-flex justify-content-center align-items-center' style={{ height: '100%', width: '100%' }}>
+                <iframe width="100%" height="100%" src={resume}></iframe>
             </div>
         </div>
     )
