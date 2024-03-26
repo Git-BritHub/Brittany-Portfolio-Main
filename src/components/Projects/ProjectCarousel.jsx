3a8a1df3fa9b8bf522ef5d3d2c002821d4014e17
Brittany Brimley <brittanysapp@gmail.com>
2024-03-26 01:46:33 -0600

    readded line of code accidentally deleted for ProjectModal to display and cleaned up code in file

--- src/components/Projects/ProjectCarousel.jsx
+++ src/components/Projects/ProjectCarousel.jsx
@@ -1,6 +1,6 @@
+import ProjectModal from "./ProjectModal";
 import React, { useState } from "react";
 import { FaEllipsis } from "react-icons/fa6";
-import ProjectModal from "./ProjectModal";
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/opacity.css';
 import './style.css';
@@ -17,53 +17,54 @@ const ProjectCarousel = ({ projects }) => {
   const openModal = (project) => {
     setSelectedProject(project);
   }
-    return (
-        <>
-          <Swiper
-            effect={'coverflow'}
-            centeredSlides={true}
-            direction={'horizontal'}
-            slidesPerView={'auto'}
-            freeMode={{
-                enabled: true,
-                momentum: true,
-                momentumBounceRatio: 3,
-                momentumRatio: 1, 
-                momentumVelocityRatio: 1,
-                sticky: true
-            }}
-            mousewheel={{
-                forceToAxis: true
-            }}
-            coverflowEffect={{
-              rotate: 50,
-              stretch: 0,
-              depth: 100,
-              modifier: 1,
-              slideShadows: true,
-            }}
-            initialSlide={1}
-            loop={true}
-            modules={[EffectCoverflow, FreeMode, Mousewheel]}
-            className="mySwiper"
-          >
+  return (
+    <>
+      <Swiper
+        effect={'coverflow'}
+        centeredSlides={true}
+        direction={'horizontal'}
+        slidesPerView={'auto'}
+        freeMode={{
+            enabled: true,
+            momentum: true,
+            momentumBounceRatio: 3,
+            momentumRatio: 1, 
+            momentumVelocityRatio: 1,
+            sticky: true
+        }}
+        mousewheel={{
+            forceToAxis: true
+        }}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        initialSlide={1}
+        loop={true}
+        modules={[EffectCoverflow, FreeMode, Mousewheel]}
+        className="mySwiper"
+        >
         {projects.map((project, index) => (
-  <SwiperSlide key={index} style={{width:'400px', height:'400px'}}>
-    <div className="slide-content-wrapper">
-      <LazyLoadImage
-        src={project.screenshot}
-        alt={project.title}
-        effect="opacity"
-      />
-      <button className="open-modal-btn" onClick={() => openModal(project)}>
-        <FaEllipsis />
-      </button>
-    </div>
-  </SwiperSlide>
-))}
+        <SwiperSlide key={index} style={{width:'400px', height:'400px'}}>
+          <div className="slide-content-wrapper">
+            <LazyLoadImage
+              src={project.screenshot}
+              alt={project.title}
+              effect="opacity"
+            />
+            <button className="open-modal-btn" onClick={() => openModal(project)}>
+              <FaEllipsis />
+            </button>
+          </div>
+        </SwiperSlide>
+        ))}
       </Swiper>
-      </>
-    );
-  };
+      {selectedProject && <ProjectModal project={selectedProject} closeModal={() => setSelectedProject(null)} />}
+    </>
+  );
+};
   
-  export default ProjectCarousel;
+export default ProjectCarousel;
--- src/components/Projects/ProjectModal.jsx
+++ src/components/Projects/ProjectModal.jsx
@@ -1,8 +1,16 @@
-function ProjectModal() {
+import './style.css';
+
+function ProjectModal({ project, closeModal }) {
     return (
-        <>
-        </>
-    )
-}
+        <div className="modal-backdrop" onClick={closeModal}>
+            <div className="modal-content" onClick={e => e.stopPropagation()}>
+                <h2>{project.title}</h2>
+                <p><a href={project.url} target="_blank" rel="noopener noreferrer">Deployed Project</a></p>
+                <p><a href={project.githubUrl} target="_blank" rel="noopener noreferrer">GitHub Repo</a></p>
+                <button onClick={closeModal}>Close</button>
+            </div>
+        </div>
+    );
+};
 
 export default ProjectModal;
