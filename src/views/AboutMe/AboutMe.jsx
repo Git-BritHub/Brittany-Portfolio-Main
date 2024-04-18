da59a3ff80d6a094980dacf3fbdbf8b9481241f8
Brittany Brimley <brittanysapp@gmail.com>
2024-04-17 23:28:44 -0600

    added/edited animation for first section on homepage

--- src/views/AboutMe/AboutMe.jsx
+++ src/views/AboutMe/AboutMe.jsx
@@ -101,11 +101,11 @@ function AboutMe() {
       <div className='container aboutMe'>
         <div className="row align-items-center">
           <div className="col-lg-6 order-lg-2 text-center">
-          <img src={profilePic} alt='Brittany Brimley - Full Stack Web Developer' className='profilePic' />
+          <img src={profilePic} alt='Brittany Brimley - Full Stack Web Developer' className='profilePic fade-in-up' />
           </div>
           <div className="col-lg-6 order-lg-1">
-            <h1>FULL STACK WEB DEVELOPER</h1>
-            <h2>
+            <h1 className="fade-in-left">FULL STACK WEB DEVELOPER</h1>
+            <h2 className="fade-in-right">
             JavaScript | TypeScript | MERN Stack | HTML | CSS | NodeJS | JSON | JQuery | ExpressJS | Handlebars | JWT | MySQL | MongoDB | GraphQL | Apollo | React | NextJS | DaisyUI | Tailwind CSS | Bootstrap | Bulma | Figma | Netlify | SwiperJS | GSAP | Google Cloud
             </h2>
           </div>
