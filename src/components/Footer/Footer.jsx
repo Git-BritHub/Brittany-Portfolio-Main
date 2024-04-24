d1a5e21645ead0381baf822a21714446360a7986
Brittany Brimley <brittanysapp@gmail.com>
2024-04-23 22:02:09 -0600

    cleaned up/edited footer to boost accessibility

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,63 +1,33 @@
-import React from 'react';
-import { FaLinkedin, FaGithub, FaStackOverflow } from 'react-icons/fa6';
-import { useState } from "react";
+import { FaLinkedin, FaGithub, FaStackOverflow } from 'react-icons/fa';
 import './style.css';
 
 export default function Footer() {
-
-    const [linkIcon, pickLinkIcon] = useState(false)
-    const [gitIcon, pickGitIcon] = useState(false)
-    const [stackIcon, pickStackIcon] = useState(false)
-
-    const stylesheet = {
-        hover: {
-            backgroundColor: 'purple',
-            border: '2px solid aqua',
-            color: 'aqua',
-            font: 'bold',
-        },
-        after: {
-            backgroundColor: 'black',
-            border: '2px solid white',
-            color: 'aqua',
-            font: 'bold',
-        },
-    };
-
     return (
         <footer className='text-center text-white bg-black w-100'>
             <div className='container-fluid px-3'>
-                <section className='my-2'>
-                    <a className='btn btn-outline-light btn floating m-1 rounded-circle'
-                        href='https://www.linkedin.com/in/brittany-brimley-390a81293/' target='_blank 'role='button'
-                        style={{...linkIcon ? stylesheet.hover : stylesheet.after
-                        }} onMouseEnter={() => {
-                            pickLinkIcon(true)
-                        }} onMouseLeave={() => {
-                            pickLinkIcon(false)
-                        }}><FaLinkedin />
-                    </a>
+                <section className='footer my-2'>
+                    {/* LinkedIn */}
                     <a className='btn btn-outline-light btn-floating m-1 rounded-circle'
-                        href='https://github.com/Git-BritHub' target='_blank' role='button'
-                        style={{...gitIcon ? stylesheet.hover : stylesheet.after
-                        }} onMouseEnter={() => {
-                            pickGitIcon(true)
-                        }} onMouseLeave={() => {
-                            pickGitIcon(false)
-                        }}><FaGithub />
+                        href='https://www.linkedin.com/in/brittany-brimley-390a81293/' target='_blank'
+                        rel='noopener noreferrer' aria-label='Visit my LinkedIn profile'>
+                        <FaLinkedin />
                     </a>
-                    {/* Create and Add StackOverflow user url */}
+
+                    {/* GitHub */}
                     <a className='btn btn-outline-light btn-floating m-1 rounded-circle'
-                        href='https://stackoverflow.com/' target='_blank' role='button'
-                        style={{...stackIcon ? stylesheet.hover : stylesheet.after
-                        }} onMouseEnter={() => {
-                            pickStackIcon(true)
-                        }} onMouseLeave={() => {
-                            pickStackIcon(false)
-                        }}><FaStackOverflow />
+                        href='https://github.com/Git-BritHub' target='_blank' 
+                        rel='noopener noreferrer' aria-label='Visit my GitHub profile'>
+                        <FaGithub />
+                    </a>
+
+                    {/* Stack Overflow */}
+                    <a className='btn btn-outline-light btn-floating m-1 rounded-circle'
+                        href='https://stackoverflow.com/' target='_blank'
+                        rel='noopener noreferrer' aria-label='Visit my Stack Overflow profile'>
+                        <FaStackOverflow />
                     </a>
                 </section>
             </div>
         </footer>
-    )
+    );
 }
